7538d4781c4da88562d9874a2d899a211e941d76
MaFernandaAguilar <104477136+MaFernandaAguilar@users.noreply.github.com>
2022-05-03 19:13:19 -0500

    Add files via upload

---
+++ Actividad4.2.js
@@ -0,0 +1,8 @@
+var num= prompt("Dame un numero");
+var con=1;
+while(con<=num){
+    if(con%5 ==0){
+    console.log(con);
+    }
+    con=con+1;
+}
---
+++ Actividad4.3.js
@@ -0,0 +1,8 @@
+var num= prompt("Dame un numero");
+i=1;
+do{
+    if(i%5 ==0){
+    console.log(i);
+    }
+    i=i+1;
+}while(i<num);
