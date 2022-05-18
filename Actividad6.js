cfdc23afabbed73aead7bbf146392412df42fa1e
MaFernandaAguilar <104477136+MaFernandaAguilar@users.noreply.github.com>
2022-05-17 21:21:24 -0500

    Add files via upload

---
+++ Actividad6.js
@@ -0,0 +1,15 @@
+var animales =["eagle","parrot","boar","lion"];
+var comingSoonAnimals=["panter", "dragon", "turtle"];
+//Concatenar los arreglos
+var concatenar =animales.concat(comingSoonAnimals);
+console.log(concatenar)
+//Acomoda el arreglo de menor a mayor
+var arr =[4,6,1,0,8,2]
+orden =arr.sort();
+console.log(orden);
+//agregar un nuevo animal
+animales.push("cow");
+console.log(animales);
+//Retirar eagle de animales
+animales.shift();
+console.log(animales);
