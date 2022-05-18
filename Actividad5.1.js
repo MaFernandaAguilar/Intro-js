c30172fb00df2f1ae43219fc80a867f0451654d3
MaFernandaAguilar <104477136+MaFernandaAguilar@users.noreply.github.com>
2022-05-17 21:20:56 -0500

    Add files via upload

---
+++ Actividad5.1.js
@@ -0,0 +1,6 @@
+var arr=[1,4,6,10,22,55,46,2,5,0];
+for(var i=0; i<arr.length-1; i++ ){
+    if(arr[i]>3){
+        console.log(arr[i]);
+    }
+}
---
+++ Actividad5.2.js
@@ -0,0 +1,8 @@
+var arr=[];
+var i=0;
+
+while(i<5){
+    arr[i]=prompt("Dame un nombre");
+    i=i+1;
+}
+console.log(arr);
